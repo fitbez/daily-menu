cec93ff7a4126c9784d33b56b4b2fb4eca6958ce
fitsum belayneh <fitsu_alt@yahoo.com>
2018-06-07 23:27:50 -0400

    getting the date

--- menu.js
+++ menu.js
@@ -11,3 +11,18 @@ var thursdayMenu = new menu('french toast', 'kikel', 'pasta');
 var fridayMenu = new menu('sandwich', 'tibse', 'pasta');
 var saturdayMenu = new menu('omlate', 'fish', 'mesere');
 var sundayMenu = new menu('eggscamble', 'shiro', 'piza');
+
+function eachDayMenu(){
+  var date = new Date();
+  var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+  
