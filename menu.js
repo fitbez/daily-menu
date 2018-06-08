ca8fa4334ab98722ce29fde713ba9704387d53bd
fitsum belayneh <fitsu_alt@yahoo.com>
2018-06-07 23:26:24 -0400

    creating new menu instance

---
+++ menu.js
@@ -0,0 +1,13 @@
+var menu = function(breakefast, lunch, dinner) {
+          this.breakefast = breakefast,
+          this.lunch = lunch,
+          this.dinner = dinner
+};
+
+var mondayMenu = new menu('sandwich', 'tibse', 'pasta');
+var tuesdayMenu = new menu('sandwich', 'mesert', 'otmeal');
+var wednesdayMenu = new menu('sandwich', 'spageti', 'pasta');
+var thursdayMenu = new menu('french toast', 'kikel', 'pasta');
+var fridayMenu = new menu('sandwich', 'tibse', 'pasta');
+var saturdayMenu = new menu('omlate', 'fish', 'mesere');
+var sundayMenu = new menu('eggscamble', 'shiro', 'piza');
